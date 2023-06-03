85f9f70fd16a6c8d1b83732238c9dd5f73455e96
eugene <eugeneholenstein@gmail.com>
2023-06-03 14:34:10 +1000

    completed TVland

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
+require 'active_record'
+
 class Character < ActiveRecord::Base
+  #A character belongs to an actor and belongs to a show
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    # return "Steve Urkel always says: Did I do that?"
+    "#{self.name} always says: #{self.catchphrase}"
+  end
   
 end
+
+
